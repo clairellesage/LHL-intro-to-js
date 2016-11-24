1220aa0786a7cbda955c048c3bab4a800bc66e43
clairellesage <clairellesage@gmail.com>
2016-11-24 20:01:23 +0000

    Initial commit with all my work so far

---
+++ function-arguments.js
@@ -0,0 +1,4 @@
+var math = function(apple, banana, celery) {
+	return apple + (banana * celery);
+};
+console.log(math(53, 61, 67));
---
+++ number-to-string.js
@@ -0,0 +1,3 @@
+var n = 128
+var stringy = n.toString()
+console.log(stringy)
